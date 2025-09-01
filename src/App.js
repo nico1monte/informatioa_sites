4d012d90eb33286ee4921a9c1f0e117c07dcdc91
Nicolas Sokolov <nicosokolov2017@gmail.com>
2025-09-01 16:29:34 -0700

    Testing with the page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project is by <code><a href="github.com/nico1monte"></a></code> Click this link to start the page.
         </p>
         <a
           className="App-link"
